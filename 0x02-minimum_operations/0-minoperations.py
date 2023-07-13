a9baa43992ce2dbee55a620a243f3349bcd36bbc
Zo3rb <ali.f90@outlook.com>
2023-07-13 16:14:18 +0000

    new solution.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python3
-'''0. Minimum Operations'''
+#!/usr/bin/python3
+"""
+0. Minimum Operations.
+"""
 
-def minOperations(n):
-	'''In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+def minOperations(n=6):
+    """
+    In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n,
+    write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
 
- 	Args:
-		n (int): Number of characters.
-		Returns: (int): Number of operations.
-	 '''
-	if not isinstance(n, int):
-		return 0
-	ops_count = 0
-	clipboard = 0
-	done = 1
-		
-	while done < n:
-		if clipboard == 0:
-			clipboard = done
-			done += clipboard
-			ops_count += 2
-		elif n - done > 0 and (n - done) % done == 0:
-			clipboard = done
-			done += clipboard
-			ops_count += 2
-		elif clipboard > 0:
-			done += clipboard
-			ops_count += 1
-	
-	return ops_count
+    Args:
+        n (int): Number of operations.
+
+    Returns:
+        (int): The fewest number of operations to perform.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while factor <= n:
+        if n % factor == 0:
+            operations += factor
+            n = n // factor
+            factor -= 1
+        factor += 1
+
+    return operations
