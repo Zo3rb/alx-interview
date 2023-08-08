841aadbc490c6b332b563a1cbc7d75ba75552c86
Zo3rb <ali.f90@outlook.com>
2023-08-08 07:29:51 +0000

    refactor

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,21 +4,22 @@
  * */
 
 const request = require('request');
+
 const movieNum = process.argv[2];
 const API_URL = `https://swapi-api.alx-tools.com/api/films/${movieNum}`;
 
-(async function getMovieCharacters (movieNum) {
-	request(API_URL, (error, response, body) => {
-		if (!error) {
-			const data = JSON.parse(body);
-			for (const charLink of data.characters) {
-				request(charLink, (error, response, body) => {
-					if (!error) {
-						console.log(JSON.parse(body).name);
-					}
-				});
-			}
-		}
-	});
-})();
+(async function getMovieCharacters() {
+  request(API_URL, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body).characters;
+      data.forEach((charLink) => {
+        request(charLink, (err, res, bod) => {
+          if (!err) {
+            console.log(JSON.parse(bod).name);
+          }
+        });
+      });
+    }
+  });
+}());
 
