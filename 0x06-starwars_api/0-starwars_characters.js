b25eaec39946849e2889fd00144e2ad94285ea5f
Zo3rb <ali.f90@outlook.com>
2023-08-11 05:55:18 +0000

    another solution.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,17 @@
 #!/usr/bin/node
-/**
- * prints all characters of a Star Wars movie
- * */
 
 const request = require('request');
-const movieNum = process.argv[2];
-const API_URL = `https://swapi-api.alx-tools.com/api/films/${movieNum}`;
 
-(async function getMovieCharacters(movieNum) {
-    request(API_URL, (error, response, body) => {
-        if (!error) {
-            let data = JSON.parse(body);
-            for (let charLink of data.characters) {
-                request(charLink, (error, response, body) => {
-                    if (!error) {
-                        body = JSON.parse(body);
-                        console.log(body.name);
-                    }
-                })
-            }
-        }
-    })
-})();
-
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
